0a546761721c91d9c9acea3a5f57c5b432677dc5
Gail <11055380@uvu.edu>
2024-09-11 18:28:06 -0600

    Sphere bouncing script attached

--- Scripting3Lab/Assets/Script/Bounce code.cs
+++ Scripting3Lab/Assets/Script/Bounce code.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Bouncecode : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float bounceForce = 3f;
+    
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GetComponent<Rigidbody>().AddForce(Vector3.up*bounceForce, ForceMode.Impulse);
     }
 }
